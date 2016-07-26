3173aec0a94b7f1df3cf45c17fdb19c3953377b9
Eduard <ekotanjyan@gmail.com>
2016-07-26 14:38:31 +0400

    fix

--- app/views/admin/footer.blade.php
+++ app/views/admin/footer.blade.php
@@ -10,10 +10,10 @@
 <!-- DataTables JavaScript -->
 <script src="{{URL::asset('asset/js/jquery.dataTables.min.js')}}"></script>
 <script src="{{URL::asset('asset/js/dataTables.bootstrap.min.js')}}"></script>
-
+ <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 <!-- Custom Theme JavaScript -->
 
-
+<script>tinymce.init({ selector:'textarea' });</script>
 <!-- Page-Level Demo Scripts - Tables - Use for reference -->
 
 </body>
